6a798f98d9b57d484a1a59a58583f1436246f983
julian-pc <julian.beutin@viacesi.fr>
2026-01-05 23:50:58 +0100

    :tada: initial project

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+
+app.listen(3000, () => {
+    console.log(`Server running on port 3000`);
+});
+
+app.get('/', (req, res) => {
+    res.send('GET request to the homepage');
+});
